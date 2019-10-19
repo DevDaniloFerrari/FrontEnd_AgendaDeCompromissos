895c8410950592a313475828862c21ac556b2c4e
DevDaniloFerrari <daniloferrari2000@hotmail.com>
2019-10-19 14:18:03 -0300

    implementando barra de navegacao

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
+import { PacienteComponent } from './paciente/paciente.component';
+import { ConsultaComponent } from './consulta/consulta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopBarComponent
+    TopBarComponent,
+    PacienteComponent,
+    ConsultaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: 'consulta', component: ConsultaComponent},
+      { path: 'paciente', component: PacienteComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
